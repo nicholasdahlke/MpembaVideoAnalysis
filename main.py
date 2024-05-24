acd1af2194d03faeac138dcbfea03500983f9a0f
Nicholas Dahlke <nicholas.dahlke@phaenovum.de>
2024-05-24 23:12:40 +0200

    Case files are now a command line argument.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import mpemba
-import datetime
+import sys
+import os
 
 if __name__ == '__main__':
+    file_list = sys.argv
+    file_list.pop(0) # Remove the code from the argument list
+    for file in file_list:
+        if not os.path.isfile(file):
+            raise Exception("One of the provided case files does not exist.")
 
-    file_list = ["/home/nicholas/Mpempa Videos/Ergebnisse neu/res1.cf",
-                 "/home/nicholas/Mpempa Videos/Ergebnisse neu/res2.cf"]
     for file in file_list:
         case = mpemba.readExperiment(file)
         exp_setup = case[0]
@@ -12,8 +16,7 @@ if __name__ == '__main__':
         video_file = case[2]
         mpemba.print_(exp_setup.initial_temperature, "Initial temperature")
         mpemba.print_(exp_setup.cooler_temperature, "Cooler temperature")
-        exp = mpemba.Experiment(video_file, exp_setup, datetime.datetime.now())
-
+        exp = mpemba.Experiment(video_file, exp_setup, time_recorded)
         mpemba.print_(exp.get_file_content("droplet_count"), "Unfrozen droplets")
         mpemba.print_(exp.get_file_content("droplet_count_frozen"), "Frozen droplets")
         mpemba.printSci(exp.get_file_content("droplet_volumes"), "Droplet volume")
@@ -22,17 +25,12 @@ if __name__ == '__main__':
         sim.simulate()
         sim_results = sim.get_results()
         supercorrector = mpemba.SupercoolingCorrector(exp, sim_results, exp_setup)
-        #supercorrector.droplet_speed = 0.017535225760000002 # manually correct as no speed was measured here
         super_time = supercorrector.get_supercooling_time()
         super_error = supercorrector.get_supercooling_error()
         res_calculator = mpemba.ResultsCalculator(exp, super_time)
-        #volume = 1.41338e-10
-        #res_calculator.volume = volume
         nucleation_rate = res_calculator.get_nucleation_rate()
         mpemba.printSci(nucleation_rate, "Nucleation rate")
         error_calculator = mpemba.ErrorCalculator(exp, super_time, super_error, nucleation_rate)
-        #error_calculator.volume_error = 1.863407635991848e-12
-        #error_calculator.volume = volume
         error = error_calculator.get_error("nonlinear")
         mpemba.printSci(error[1], "Positive error")
         mpemba.printSci(error[0], "Negative error")
