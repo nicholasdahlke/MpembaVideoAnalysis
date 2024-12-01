e8f5f74bca4e88f5a6a7ee96b655e09ca1e17524
Nicholas Dahlke <nicholas.dahlke@phaenovum.de>
2024-12-01 12:54:11 +0100

    Things

--- PyDataEvaluation/mpemba_plot.py
+++ PyDataEvaluation/mpemba_plot.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     nucleation_rate = []
     error_pos = []
     error_neg = []
+    droplets = []
 
     for file in file_list:
         with open(file, "r") as toml_file:
@@ -26,9 +27,20 @@ if __name__ == '__main__':
             nucleation_rate.append(toml_dict["results"]["nucleation_rate"])
             error_pos.append(toml_dict["results"]["error_pos"])
             error_neg.append(toml_dict["results"]["error_neg"])
+            droplets.append(toml_dict["results"]["droplets"] + toml_dict["results"]["droplets_frozen"])
 
     #plt.errorbar(initial_temperature, nucleation_rate, yerr=(error_neg, error_pos), linestyle="")
-    plt.scatter(initial_temperature, nucleation_rate)
-    plt.xlabel("$T_h$")
+    plt.scatter(initial_temperature, nucleation_rate, c=droplets)
+    cbar = plt.colorbar()
+    cbar.set_label("$N_0$")
     plt.ylabel(r"$\dot{N}$")
+    plt.xlabel("$T_h$")
+
+
+    plt.figure()
+    plt.scatter(range(len(initial_temperature)), nucleation_rate, c=initial_temperature)
+    cbar = plt.colorbar()
+    cbar.set_label("$T_h$")
+    plt.ylabel(r"$\dot{N}$")
+    plt.xlabel("Messung")
     plt.show()
